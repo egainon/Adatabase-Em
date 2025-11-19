20fbac53736473b57ba757d13dae5c347c566021
egainon <miliegainon@hotmail.fr>
2025-11-19 14:53:41 +0100

    Tables+types create

---
+++ migration_up.sql
@@ -0,0 +1,42 @@
+-- Create un type
+CREATE TYPE resource_type AS ENUM (
+    'guide',
+    'video',
+    'exercice',
+    'projet'
+);
+
+-- -- Create a new table 'themes' with a primary key and columns
+CREATE TABLE themes (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+-- Create a new table 'ressources' with a primary key and columns
+CREATE TABLE resources (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    url TEXT NOT NULL,
+    description TEXT NOT NULL,
+    type resource_type,
+    is_ada BOOLEAN,
+    theme_id INTEGER REFERENCES themes(id), 
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+-- -- Create a new table 'skills' with a primary key and columns
+CREATE TABLE skills (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+-- Create a new table 'ressources_skills' with a primary key and columns
+CREATE TABLE resources_skills (
+    resource_id INTEGER REFERENCES resources(id), 
+    skill_id INTEGER REFERENCES skills(id)
+);
+
